c6698e422084b1547e4ce30b5b3b18bf325d6b1e
Marc Rodríguez <nebrios2@gmail.com>
2017-10-07 17:23:06 +0200

    Add simple note input

--- App.elm
+++ App.elm
@@ -1,6 +1,8 @@
 import Html exposing (..)
 import Notes exposing (Note, Tag)
 import ViewHelpers exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 main = Html.beginnerProgram
   { model = model
@@ -12,30 +14,51 @@ main = Html.beginnerProgram
 
 type alias Model =
   { notes: List Note
+  , newNoteTitle: String
   }
 
 model: Model
-model = Model [Note (Tag "Chapter") [] "Chapter"]
+model = Model [Note (Tag "Chapter") [] "Chapter"] ""
 
 -- UPDATE
 
-type Msg =
-  AddNote Note
+type Msg = AddNote | ChangeNewNoteTitle String
+
 
 update: Msg -> Model -> Model
 update msg model =
-  {model | notes = notes msg model.notes}
+  {model |
+    notes = notes msg model.notes model,
+    newNoteTitle = newNoteTitle msg model.newNoteTitle
+  }
 
-notes: Msg -> List Note -> List Note
-notes msg model =
+notes: Msg -> List Note -> Model -> List Note
+notes msg state model =
   case msg of
-    AddNote note -> note :: model
+    AddNote -> List.append state [createNote model]
+    _ -> state
+
+createNote: Model -> Note
+createNote model =
+  let
+    newNoteTitle = model.newNoteTitle
+  in
+    Note (Tag newNoteTitle) [] newNoteTitle
+
+newNoteTitle: Msg -> String -> String
+newNoteTitle msg state =
+  case msg of
+    ChangeNewNoteTitle s -> s
+    AddNote -> ""
 
 -- VIEW
 
 view: Model -> Html Msg
 view model =
-  listView model.notes
+  div []
+  [ listView model.notes
+  , addNoteInput model.newNoteTitle
+  ]
 
 listView: List Note -> Html Msg
 listView notes =
@@ -43,6 +66,13 @@ listView notes =
 
 noteView: Note -> Html Msg
 noteView note =
-  div [] [
-    text note.title
+  div []
+  [ text note.title
   ]
+
+addNoteInput: String -> Html Msg
+addNoteInput newNoteTitle =
+  div []
+    [ input [onInput ChangeNewNoteTitle, value newNoteTitle] []
+    , button [onClick AddNote] [text "+"]
+    ]
